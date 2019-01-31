522ed6f633a785052cd58a1035b06698d48f1491
izzyzizou <izzyzizou@ymail.com>
2019-01-31 14:21:34 -0500

    introduced react bootstrap and updated images with correct format

--- src/Component/StarWarsApi.jsx
+++ src/Component/StarWarsApi.jsx
@@ -1,16 +1,27 @@
 import React, { Component } from 'react';
 import characters from '../characters.json';
-import {Button, Card} from 'react-bootstrap';
-import ls from '../imgs/luke_skywalker.jpg';
+import {Button, Card, Row, Col, Container} from 'react-bootstrap';
+import lukeImg from '../imgs/luke_skywalker.jpg';
+import obiwan from '../imgs/obi_wan_kenobi.jpg';
+import r2_d2 from '../imgs/r2_d2.jpg';
 
 class StarWarsApi extends Component{
+    
     constructor(props) {
         super(props);
         this.state = {
             error: null,
             isLoaded: false,
-            items: []
+            items: [],
+            isToggleOn: true
         };
+        // This binding is necessary to make `this` work in the callback
+        this.handleClick = this.handleClick.bind(this);
+    }
+    handleClick(){
+        this.setState(state => ({
+            isToggleOn: !state.isToggleOn
+        }));
     }
     componentDidMount(){
         //User selects a character
@@ -42,16 +53,60 @@ class StarWarsApi extends Component{
     }
     render(){
         const { error, isLoaded, items } = this.state;
-        if (error) {
-            return <div>Error: {error.message}</div>;
-          } else if (!isLoaded) {
-            return <div>Loading...</div>;
-          } else {
+        // if (error) {
+        //     return <div>Error: {error.message}</div>;
+        //   } else if (!isLoaded) {
+        //     return <div>Loading...</div>;
+        //   } else {
             return(
                 <div>
-                    <Button variant="primary">Luke Skywalker</Button>
+                    <Button variant="danger" onClick={this.handleClick}>
+                        {this.state.isToggleOn ? 'ON' : 'OFF'}
+                    </Button>
+                    <Container>
+                        <Row>
+                            <Col>
+                                <Card style={{ width: '18rem' }}>
+                                    <Card.Img variant="top" src={lukeImg} />
+                                    <Card.Body>
+                                        <Card.Title>Luke Skywalker</Card.Title>
+                                        <Card.Text>
+                                            Some quick example text to build on the card title and make up the bulk of
+                                            the card's content.
+                                        </Card.Text>
+                                        <Button name="luke" variant="primary">View Films</Button>
+                                    </Card.Body>
+                                </Card>
+                            </Col>
+                            <Col>
+                                <Card style={{ width: '18rem' }}>
+                                    <Card.Img variant="top" src={obiwan} />
+                                    <Card.Body>
+                                        <Card.Title>Obi Wan Kenobi</Card.Title>
+                                        <Card.Text>
+                                            Some quick example text to build on the card title and make up the bulk of
+                                            the card's content.
+                                        </Card.Text>
+                                        <Button variant="primary">View Films</Button>
+                                    </Card.Body>
+                                </Card>
+                            </Col>
+                            <Col>
+                                <Card style={{ width: '18rem' }}>
+                                    <Card.Img variant="top" src={r2_d2} />
+                                    <Card.Body>
+                                        <Card.Title>R2-D2</Card.Title>
+                                        <Card.Text>
+                                            Some quick example text to build on the card title and make up the bulk of
+                                            the card's content.
+                                        </Card.Text>
+                                        <Button variant="primary">View Films</Button>
+                                    </Card.Body>
+                                </Card>
+                            </Col>
+                        </Row>
+                    </Container>
                     {console.log(items["title"], items["release_date"])}
-                    <p>Hello world</p>
                 </div>
                 
                 // <ul>
@@ -62,7 +117,7 @@ class StarWarsApi extends Component{
                 //     ))}
                 // </ul>
             )
-        }
+        // }
     }
 }
 
